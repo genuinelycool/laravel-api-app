4ea5ffa656ae75dab9dd31344714e50386b5cee9
genuinelycool <genuinelycool@gmail.com>
2025-10-26 22:19:38 +0545

    user login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -27,7 +28,25 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        return 'login';
+        $fields = $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return [
+                'message' => 'The provided credentials are incorrect.'
+            ];
+        }
+
+        $token = $user->createToken($user->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
     }
 
     public function logout(Request $request)
