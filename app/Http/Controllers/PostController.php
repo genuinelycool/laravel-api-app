5cdb5fbbfdb15d61f56e8e968812b86c05b90cbd
genuinelycool <genuinelycool@gmail.com>
2025-10-26 16:36:16 +0545

    route for api resource and define PostController methods

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StorePostRequest;
-use App\Http\Requests\UpdatePostRequest;
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -13,15 +12,23 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return Post::all();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePostRequest $request)
+    public function store(Request $request)
     {
-        //
+        $fields =$request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ]);
+
+        $post = Post::create($fields);
+
+        // return ['post' => $post];
+        return $post;
     }
 
     /**
@@ -29,15 +36,23 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        // return ['post' => $post];
+        return $post;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(Request $request, Post $post)
     {
-        //
+        $fields =$request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ]);
+
+        $post->update($fields);
+
+        return $post;
     }
 
     /**
@@ -45,6 +60,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return [ 'message' => 'The post was deleted'];
     }
 }
